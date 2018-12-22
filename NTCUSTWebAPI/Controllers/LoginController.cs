dd0297ce91971170782ed618f15a597613bf7836
黃俊霖 <elase2000@Hotmail.com>
2018-12-22 09:31:09 +0800

    加入登入檢查功能。

---
+++ NTCUSTWebAPI/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using NTCUSTWebAPI.Model;
+using NTCUSTWebAPI.Models;
+using Swashbuckle.Examples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NTCUSTWebAPI.Controllers
+{
+    [RoutePrefix("Login")]
+    public class LoginController : ApiController
+    {
+        [Route("")]
+        [SwaggerRequestExample(typeof(LoginModel), typeof(LoginModelExample))]
+        public IHttpActionResult Post([FromBody] LoginModel Login)
+        {
+            if (((Login.Account == "Tom") && (Login.Password == "123456")) ||
+                ((Login.Account == "Mary") && (Login.Password == "111111")))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("帳號密碼錯誤");
+            }
+        }
+    }
+}
---
+++ NTCUSTWebAPI/Models/LoginModel.cs
@@ -0,0 +1,27 @@
+﻿using Swashbuckle.Examples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTCUSTWebAPI.Models
+{
+    public class LoginModel
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+    }
+
+
+    public class LoginModelExample : IExamplesProvider
+    {
+        public object GetExamples()
+        {
+            return new LoginModel
+            {
+                Account = "Tom",
+                Password = "123456"
+            };
+        }
+    }
+}
